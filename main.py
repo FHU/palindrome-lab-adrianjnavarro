a1a24d1da1afcf0fbe189d59ddcdbfdc70038469
Adrian <adrian.navarro@students.fhu.edu>
2024-02-01 13:33:19 -0500

    Update main.py
    yippee

--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 #REMOVE PASS AND FIX THIS FUNCTION
 def palindrome(word):
-    pass
+    cleaned_word = ''.join(char.lower() for char in word if char.isalnum())
+    return cleaned_word == cleaned_word [::-1]
+
+if __name__ == '__main__':
+    user_input = input()
+    result = palindrome(user_input)
+
+
+    if result:
+        print("True")
+    else:
+        print("False")
+
 
 #YOUR CODE GOES HERE
